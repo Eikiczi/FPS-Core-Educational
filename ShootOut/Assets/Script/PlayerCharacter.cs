3a58aab273269407f6d1832e9356066721a4a081
Dawid Lipka <dawidlipka91@gmail.com>
2019-02-15 16:43:25 +0100

    FirstCommit
    Added Unity project to GitHub

---
+++ ShootOut/Assets/Script/PlayerCharacter.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCharacter : MonoBehaviour
+{
+
+    public int _health;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _health = 3;
+    }
+
+    public void Hurt(int damage) {
+        _health -= damage;
+        Debug.Log("Zycie gracza " + _health);
+    }
+}
---
+++ ShootOut/Assets/Script/RayShooter.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RayShooter : MonoBehaviour
+
+
+{
+
+    private Camera _camera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _camera = GetComponent<Camera>();
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+    void OnGUI() {
+        int size = 12;
+        float posX = _camera.pixelWidth/2 - size/4;
+        float posY = _camera.pixelHeight/2 - size/2;
+        GUI.Label(new Rect(posX, posY, size, size), "*");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if(Input.GetButtonDown("Fire1")){
+            if(Input.GetMouseButtonDown(0)){
+            //środek kamery
+            Vector3 point = new Vector3(_camera.pixelWidth/2, _camera.pixelHeight/2, 0);
+            //zbadaj proces
+            Ray ray  =_camera.ScreenPointToRay(point);
+            RaycastHit hit;
+            //pokaż gdzie
+            if(Physics.Raycast(ray, out hit)){
+                Debug.Log("Hit" + hit.point);
+                Debug.Log("Name: " + hit.transform.gameObject.name);
+                GameObject hitObject = hit.transform.gameObject;
+                ReactiveTarget target = hitObject.GetComponent<ReactiveTarget>();
+                if(target != null)
+                {
+                    target.ReactToHit();
+                }
+                else
+                {
+                    StartCoroutine(SphereIndicator(hit.point));
+                }
+                
+            }
+        }
+    }
+
+    private IEnumerator SphereIndicator(Vector3 pos){
+        GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        sphere.transform.position = pos;
+        yield return new WaitForSeconds(1);
+        Destroy(sphere);
+    }
+}
